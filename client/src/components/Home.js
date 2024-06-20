1ec5aa112e61670600e72c70cb38539d63117925
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-06-20 13:19:20 +0200

    Login update

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -54,7 +54,7 @@ const LoggedOutHome = () => {
             <div className="container">
               <h1>Welcome to YouDemy</h1>
               <p>Your journey to amazing learning starts here.</p>
-              <Link to='/Playlists' className="btn">Get Started</Link>
+              <Link to='/Login' className="btn">Get Started</Link>
             </div>
           </section>
   
